c1ca0e74ee0d34dc42ee00b51845e6cb2318efec
xiyangyang <2101093918@qq.com>
2022-11-06 14:41:34 +0800

    一个关机小程序

---
+++ c_c++_code/test.c
@@ -0,0 +1,75 @@
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//    char input[10] = { 0 };
+//    system("shutdown -s -t 120");
+//        again:
+//    printf("电脑将在2分钟内关机,如果输入:123,就取消关机!\n:>");
+//        scanf_s("%s", input,3);
+//    if (0 == strcmp(input, "123"))
+//    {
+//        system("shutdown -a");
+//    }
+//    else
+//    {
+//        goto again;
+//    }
+//    return 0;
+//}
+
+//int main()
+//{
+//    char input[10] = { 0 };
+//    system("shutdown -s -t 120");
+//    while (1)
+//    {
+//        printf("电脑将在1分钟内关机，如果输入：我是猪，就取消关机!\n请输入:>");
+//        scanf_s("%s", input,6);
+//        if (0 == strcmp(input, "我是猪"))
+//        {
+//            system("shutdown -a");
+//            break;
+//        }
+//    }
+//    return 0;
+//}
+
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	int n = strlen("我是猪");
+//	printf("%d", n);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include<string.h>
+//#include <stdlib.h>
+//int main()
+//{
+//    char input[10] = { 0 };
+//    system("shutdown -s -t 120");
+//    while (1)
+//    {
+//        printf("电脑将在2分钟内关机，如果输入：123，就取消关机!\n请输入:>");
+//        scanf_s("%s", input,3);
+//        if (0 == strcmp(input, "123"))
+//        {
+//            system("shutdown -a");
+//            break;
+//        }
+//    }
+//    return 0;
+//}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	scanf("%d", &a);
+	return 0;
+}
